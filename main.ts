30633bb5b795b847f29f8590de2d66cc438df4aa
Santiago Jimenez Cano <santiagojcano28@gmail.com>
2025-07-25 12:18:34 -0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   // Habilitar CORS para desarrollo
   app.enableCors({
-    origin: ['https://test-front-fcwugvfhe-santijcanos-projects.vercel.app/'], 
+    origin: ['https://test-front-fcwugvfhe-santijcanos-projects.vercel.app'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
